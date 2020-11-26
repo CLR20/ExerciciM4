e243bf89abd509a74ff085882ebb3aeb6ee6dd04
CLR20 <72393450+CLR20@users.noreply.github.com>
2020-11-26 16:16:06 +0100

    Fase1

---
+++ Exercicis Java M4/src/Fase1App.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Fase1App {
+
+	public static void main (String[] args) {
+		
+		
+		// Construim les variables pels bitllets, i la que contindrà el total.
+				
+		int cinc = 5;
+		int deu = 10;
+		int vint = 20;
+		int cinquanta = 50;
+		int cent = 100;
+		int doscents = 200;
+		int cinccents = 500;
+		
+		int total = 0;
+		
+		// Construim les variables que contindran el menú i el preu.
+		
+		Object [] menu = new Object [9];
+		Object [] preu = new Object [9];
+	
+	}
+	
+}
